63ddceacf71d8eaa929edd8b1fe7d262ab0f8718
gabrielcordeiro13-bit <gabriel.cordeiro13@escola.pr.gov.br>
2026-08-27 11:19:34 +0000

    Salvamento 05

--- experimento06.js
+++ experimento06.js
@@ -2,5 +2,18 @@ const botaoAbrir = document.getElementById("btnAbrirModal");
 const modalContainer = document.getElementById("modalContainer");
 
 botaoAbrir.addEventListener("click", function(){
-    
-})
+    modalContainer.innerHTML = `
+      <div class="modal-fundo">
+       <div class="modal-caixa">
+         <h2>Aviso!</h2>
+           <p> Este modal inteiro foi criado pelo JavaScript.</p>
+           <button id="btnFechaModal">Fechar</button>
+        <div>
+    <div>`;
+
+    const botaoFechar = document.getElementById("btnFechaModal");
+    botaoFechar.addEventListener("click", function(){
+        modalContainer.innerHTML = "";
+    })
+
+});
