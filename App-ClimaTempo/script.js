25375435a8922ecfe844cc6b916db2ce9d3d8a94
gabrielcordeiro13-bit <gabriel.cordeiro13@escola.pr.gov.br>
2026-09-17 11:54:14 +0000

    SALVAMENTO 07 -  REOGARNICAÇÃO DO JS

--- App-ClimaTempo/script.js
+++ App-ClimaTempo/script.js
@@ -38,41 +38,7 @@ botaoBuscar.addEventListener("click", async function(){
             <p>Sensação: ${dados.main.feels_like}ºC</p>
         </div>
     `;
-        localStorage.seasync function buscarclima(cidade){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
-    const resposta = await fetch(url);
-    const dados = await resposta.json()
-    return dados; // tem que retornar os dados. Agora a função entrega a resposta
-}
-
-const botaoBuscar = document.getElementById("btnBuscar");
-const campoCidade = document.getElementById("cidade");
-const divResultado = document.getElementById("resultado");
-
-botaoBuscar.addEventListener("click", async function(){
-    const cidade = campoCidade.value;
-
-    if (cidade == ""){
-        return;
-    }
-
-    const dados = await buscarclima(cidade);
-
-    if (dados.cod == "404"){
-        divResultado.innerHTML = "<p> Cidade não encontrada.</p>";
-        return;
-    }
-
-    divResultado.innerHTML = `
-        <div class="card-clima">
-            <h3>${dados.name}</h3>
-            <p>${dados.weather[0].description}</p>
-            <p><strong>${dados.main.temp}ºC</strong></p>
-            <p>Sensação: ${dados.main.feels_like}ºC</p>
-        </div>
-    `;
-
-});tItem("ultimaCidade",cidade)
+        localStorage.setItem("ultimaCidade",cidade)
 
         const previsao = await buscarPrevisao(cidade);
         montarPrevisao(cidade);
